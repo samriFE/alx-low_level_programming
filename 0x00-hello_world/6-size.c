6a8418a6d2d6a3c25951c552cdf36ad0e5c91f5e
samriFE <samrawifetene89@gmail.com>
2022-06-06 14:11:29 -0700

    size done

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*
+*main -Entry point
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+printf("size of a char : %d byte(s)/n",sizeof(char);
+printf(sizeof(int));
+printf(sizeof(iong int));
+printf(sizeoflong long int));
+return (0);
+}
