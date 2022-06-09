9b2508906db0f369609f3f0f027bef19e82877d3
samriFE <samrawifetene89@gmail.com>
2022-06-09 10:47:57 -0700

    my commit message

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _isupper - evalue if letteris uppercase .
+*@C: print int
+*Return: Always 0.
+*/
+int _isdigit(int c);
+{
+if (c >= 'A' && c <= 'Z' )
+{
+return(1);
+}
+else
+{
+return(0);
+}
+
+}
