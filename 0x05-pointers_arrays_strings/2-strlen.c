ca9650baf4d1e390b1a8fed6c3f37673688b9741
samriFE <samrawifetene89@gmail.com>
2022-06-13 05:48:58 -0700

    my secound commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,47 +1,20 @@
 #include "main.h"
 
 /**
- * is_numerical - check if it is a digit
- * @n: Number
- * Return: If is a number, return 1 else return 0
+ *_strlen -length of a string
+ *@s: A pointer to an int that will be changed/updated
+ *
+ *Return: void that means our answer is correct
  */
-int is_numerical(unsigned int n)
+
+int _strlen(char *s)
 {
-return (n >= '0' &&  n <= '9');
+int u;
+
+u = 0;
+while (s[u] != '\0')
+{
+u++;
 }
-
-/**
- * _atoi - convert a string to an integer
- *@s: String
- * Return: Return the num
- */
-int _atoi(char *s)
-{
-unsigned int number, i;
-int sign;
-
-sign = 1;
-number = 0;
-
-
-
-for (i = 0; s[i] != '\0'; i++)
-{
-if (is_numerical(s[i]))
-{
-number = (s[i] - 48) + number * 10;
-
-if (s[i + 1] == ' ')
-break;
-}
-else if (s[i] == '-')
-{
-sign *= -1;
-}
-
-}
-
-return (number *sign);
-
-
+return (u);
 }
